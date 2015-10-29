5c274ee759c6c58423b03dfdb44edcbbd719162f
Will Andrews <will@firepipe.net>
2015-10-28 19:41:38 -0600

    More quick upgrade scaffolding.

--- vig-common/upgrade.subr.sh
+++ vig-common/upgrade.subr.sh
@@ -16,27 +16,40 @@ guest_upgrade() {
 }
 register_command guest upgrade "Upgrade the guest using built sources"
 
-host_upgrade_guest() {
+guest_quick_upgrade() {
+	cd $GUEST_WS
+	sudo $GUEST_WS/usr/src/tools/quick/make-zfs onuzfs
+	upgrade_epilogue
+}
+register_command guest quick_upgrade "Use make-zfs to do a quick upgrade"
+
+upgrade_prologue_host() {
 	upgrade_prologue
 	OPTS="$1"; shift
-
 	# Allow specifying "-" as the BE name to generate a date stamp.
 	[ "$BENAME" = "-" ] && BENAME="$(date -u '+%Y.%m.%d.%H%M%S')"
 
 	host_startvm
 	runcmd vagrant snap take default --name pre-upgrade-$BENAME
-	runguest upgrade $BENAME
-	runcmd vagrant reload
+}
 
+upgrade_epilogue_host() {
 	[ "$OPTS" != "nopostsnap" ] &&
 		runcmd vagrant snap take default --name post-upgrade-$BENAME
 }
+
+host_upgrade_guest() {
+	upgrade_prologue_host
+	runguest upgrade $BENAME
+	runcmd vagrant reload
+	upgrade_epilogue_host
+}
 register_command host upgrade_guest "Upgrade the guest using built sources"
 
-guest_quick_upgrade() {
-	upgrade_prologue
-	cd $GUEST_WS
-	sudo $GUEST_WS/usr/src/tools/quick/make-zfs onuzfs $BENAME
-	upgrade_epilogue
+host_quick_upgrade() {
+	upgrade_prologue_host
+	runguest quick_upgrade
+	runcmd vagrant reload
+	upgrade_epilogue_host
 }
-register_command host quick_upgrade "Use make-zfs to do a quick upgrade"
+register_command host quick_upgrade "Wrap the guest quick upgrade"
